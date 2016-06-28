0672b69ac48d675b1bd18d9f11ba4fbf0bf6bc98
SamGiumelli <youtube_ninjatendo@hotmail.com>
2016-06-28 17:01:29 +1000

    [imported charSelector and charController]

---
+++ Integration Project/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuManager : MonoBehaviour 
+{
+	[System.Serializable]
+	public class MenuItemValues
+	{
+		public string heroName;
+		public float damage;
+		public float moveSpeed;
+		public float attackSpeed;
+		public float health;
+		public Sprite menuSprite;
+		public Color menuSpriteColor;
+		public Sprite menuIconSprite;
+		public GameObject heroPrefab;
+	}
+
+	public GameObject menuParent;
+	public GameObject menuItemPrefab;
+	public Canvas menuCanvas;
+
+
+	public MenuItemValues[] menuItems;
+
+	void Start()
+	{
+		for (int i = 0; i < menuItems.Length; i++) 
+		{
+			GameObject newMenuItem = Instantiate (menuItemPrefab) as GameObject;
+
+			newMenuItem.transform.SetParent(menuParent.transform);
+
+			newMenuItem.transform.localScale = Vector3.one;
+
+			MenuItem menuItem = newMenuItem.GetComponent<MenuItem> ();
+
+			menuItem.SetMenuItem (menuItems [i].damage, menuItems [i].moveSpeed, menuItems [i].attackSpeed, menuItems [i].health, menuItems [i].heroName, menuItems [i].menuSprite, menuItems[i].menuSpriteColor, menuItems [i].menuIconSprite, menuItems[i].heroPrefab, menuCanvas);
+		}
+	}
+}
