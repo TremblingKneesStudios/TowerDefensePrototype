bf20b4d59f03470b31fe33f50402971df13da152
ColinSik <colinsiktafe@gmail.com>
2016-06-28 23:55:09 +1000

    Optimal Zone

---
+++ OptimalZonePrototype/OptimalZonePrototype/Assets/test.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour
+{
+    public OptimalZone optimalZone;
+
+    void Start()
+    {
+        optimalZone.onTowerDrop(gameObject);
+    }
+}
