2162c3edceabfd555ec64fc95c5c273715554ad3
Jake Crew <jake.crew21@gmail.com>
2016-06-28 16:25:36 +1000

    [importing tower mech and enemy att]

---
+++ TowerTest/Assets/scripts/EnemyManager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyManager : MonoBehaviour {
+    public float health = 100f;
+    private float power = 20f;
+    private bool canAtt;
+    private float delay = 1f;
+    private float range = 0.5f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (health <= 0) {
+            Destroy(gameObject);
+        }
+	}
+    void Attacking() {
+        GameObject[] towers;
+        float dist;
+        if (canAtt) {
+            towers = GameObject.FindGameObjectsWithTag("Towers");
+            for (int i = 0; i < towers.Length; i++) {
+                dist = Vector3.Distance(gameObject.transform.position, towers[i].transform.position);
+                if (dist <= range) {
+                    towers[i].GetComponent<TowerManager>().curHealth -= power;
+                    if (towers[i].GetComponent<TowerManager>().curHealth > 0) {
+                        i--;
+                    }
+                }
+            }
+            StartCoroutine(AttCooldown(canAtt));
+        }
+    }
+    IEnumerator AttCooldown(bool CA) {
+        CA = false;
+        yield return new WaitForSeconds(delay);
+        CA = true;
+        StopCoroutine("AttCooldown");
+    }
+}
---
+++ TowerTest/Assets/scripts/PlayerResources.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerResources : MonoBehaviour {
+    public float crystal;
+    public float ironOre;
+    public float elixir;
+    public float essence;
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
