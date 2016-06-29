be13df6eb6c48bf7302a8160026c68cec55f4f8e
Jake Crew <jake.crew21@gmail.com>
2016-06-29 14:37:22 +1000

    [enemy att updated] commented out line in function to target player

--- TowerTest/Assets/scripts/EnemyManager.cs
+++ TowerTest/Assets/scripts/EnemyManager.cs
@@ -1,4 +1,4 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class EnemyManager : MonoBehaviour {
@@ -7,9 +7,11 @@ public class EnemyManager : MonoBehaviour {
     private bool canAtt;
     private float delay = 1f;
     private float range = 0.5f;
+	public NavMeshAgent myAgent;
 	// Use this for initialization
 	void Start () {
-	
+		myAgent = gameObject.GetComponent<NavMeshAgent>();
+		myAgent.SetDestination(GameObject.FindGameObjectWithTag("Player").transform.position);
 	}
 	
 	// Update is called once per frame
@@ -17,28 +19,37 @@ public class EnemyManager : MonoBehaviour {
         if (health <= 0) {
             Destroy(gameObject);
         }
+		Attacking();
 	}
     void Attacking() {
         GameObject[] towers;
+        GameObject player;
         float dist;
+        float playerDist;
         if (canAtt) {
             towers = GameObject.FindGameObjectsWithTag("Towers");
+            player = GameObject.FindGameObjectWithTag("Player");
             for (int i = 0; i < towers.Length; i++) {
                 dist = Vector3.Distance(gameObject.transform.position, towers[i].transform.position);
-                if (dist <= range) {
-                    towers[i].GetComponent<TowerManager>().curHealth -= power;
-                    if (towers[i].GetComponent<TowerManager>().curHealth > 0) {
-                        i--;
+                playerDist = Vector3.Distance(gameObject.transform.position, player.transform.position);
+                if (dist <= range || playerDist <= range) {
+                    if (dist <= range) {
+                        towers[i].GetComponent<TowerManager>().curHealth -= power;
+                        if (towers[i].GetComponent<TowerManager>().curHealth > 0) {
+                            i--;
+                        }else if (playerDist <= range) {
+                            //player.GetComponent<HeroStats>().health -= power;
+                        }
                     }
                 }
             }
             StartCoroutine(AttCooldown(canAtt));
         }
     }
-    IEnumerator AttCooldown(bool CA) {
-        CA = false;
+    IEnumerator AttCooldown(bool canAtt) {
+        canAtt = false;
         yield return new WaitForSeconds(delay);
-        CA = true;
+        canAtt = true;
         StopCoroutine("AttCooldown");
     }
 }
